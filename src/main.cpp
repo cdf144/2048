e4beebcb195d7e94ec4006f45c50a5c4a2d6c8bf
TDP <trandinhphu6a@gmail.com>
2023-04-08 14:01:18 +0700

    added classes and some basic functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,52 @@
 using std::cin;
 using std::cout;
 
+const int GRID_SIZE = 4;
+
+class Tile {
+    public:
+        int val{ 0 };
+};
+
+class Grid {
+    public:
+        Tile* board[GRID_SIZE][GRID_SIZE];
+        Grid() {
+            for (int i=0; i<GRID_SIZE; i++) {
+                for (int j=0; j<GRID_SIZE; j++)
+                    board[i][j] = NULL;
+            }
+        }
+
+        void insertTileVal(int val, int row, int col) {
+            board[row][col] = new Tile;
+            board[row][col]->val = val;
+        }
+        
+        void printGrid() {
+            for (int i=0; i<GRID_SIZE; i++) {
+                for (int j=0; j<GRID_SIZE; j++) {
+                    if (board[i][j] == NULL) cout << "-" << ' ';
+                    else cout << board[i][j]->val << ' ';
+                } 
+                cout << "\n";
+            }
+        }
+        ~Grid() {
+            for (int i=0; i<GRID_SIZE; i++) {
+                for (int j=0; j<GRID_SIZE; j++)
+                    delete board[i][j];
+            }
+            cout << "Grid destroyed" << std::endl;
+        }
+};
+
+
+
+bool isGameOver() {return 1;}
+
 int main() {
-    cout << "Hello World!";
+    Grid playground;
+    playground.printGrid();
     return 0;
 }
