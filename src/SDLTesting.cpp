4099939719fee3d6ff1da3c275bb8c2b671ed93f
TDP <trandinhphu6a@gmail.com>
2023-04-26 17:21:21 +0700

    Splitted files, modified makefile

--- src/SDLTesting.cpp
+++ src/SDLTesting.cpp
@@ -2,15 +2,51 @@
 #include <stdio.h>
 
 const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 640;
+const int SCREEN_HEIGHT = 480;
 
 bool init();
 bool loadMedia();
 void close();
 
-SDL_Window* gWindow = nullptr;
-SDL_Surface* gScreenSurface = nullptr;
-SDL_Surface* gHelloWorld = nullptr;
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+bool init() {
+    bool success = true;
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+        printf( "SDL could not initialize!\nSDL_Error: %s", SDL_GetError() );
+        success = false;
+    }
+    else {
+        gWindow = SDL_CreateWindow( "SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if( gWindow == nullptr ) {
+            printf( "Window could not be created!\nSDL_Error: %s", SDL_GetError() );
+            success = false;
+        }
+        else
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+    }
+    return success;
+}
+
+bool loadMedia() {
+	bool success = true;
+    gHelloWorld = SDL_LoadBMP( "media/hello_world.bmp" );
+    if( gHelloWorld == nullptr ) {
+        printf( "Unable to load image %s!\nSDL_Error: %s", "media/hello_world.bmp", SDL_GetError() );
+        success = false;
+    }
+    return success;
+}
+
+void close() {
+	SDL_FreeSurface( gHelloWorld );
+    gHelloWorld = NULL;
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+    SDL_Quit();
+}
 
 int main( int argc, char* argv[] ) {
     if ( !init() )
@@ -32,40 +68,4 @@ int main( int argc, char* argv[] ) {
     }
     close();
     return 0;
-}
-
-bool init() {
-    bool success = true;
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-        printf( "SDL could not initialize!\nSDL_Error: %s", SDL_GetError() );
-        success = false;
-    }
-    else {
-        gWindow = SDL_CreateWindow( "SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if( gWindow == nullptr ) {
-            printf( "Window could not be created!\nSDL_Error: %s", SDL_GetError() );
-            success = false;
-        }
-        else
-            gScreenSurface = SDL_GetWindowSurface( gWindow );
-    }
-    return success;
-}
-
-bool loadWindow() {
-    bool success = true;
-    gHelloWorld = SDL_LoadBMP( "media/hello_world.bmp" );
-    if( gHelloWorld == nullptr ) {
-        printf( "Unable to load image %s!\nSDL_Error: %s", "media/hello_world.bmp", SDL_GetError() );
-        success = false;
-    }
-    return success;
-}
-
-void close() {
-    SDL_FreeSurface( gHelloWorld );
-    delete gHelloWorld;
-    SDL_DestroyWindow( gWindow );
-    delete gWindow;
-    SDL_Quit();
 }
---
+++ src/include/Grid.h
@@ -0,0 +1,19 @@
+#define Grid_h
+
+#include <iostream>
+#include <cstdlib>
+
+const int GRID_SIZE = 4;
+
+struct Grid {
+    int* board[GRID_SIZE][GRID_SIZE];
+    Grid();
+    void printGrid();
+    void generate();
+    bool isGameOver();
+    void moveRight();
+    void moveLeft();
+    void moveUp();
+    void moveDown();
+    ~Grid();
+};
