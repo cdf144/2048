bb4881e538fce4e3bce4a29743071bd9645d3e94
TDP <trandinhphu6a@gmail.com>
2023-04-30 20:00:15 +0700

    game loop now handled through a separate function, added losing announcement

--- src/main.cpp
+++ src/main.cpp
@@ -6,42 +6,61 @@
 using std::cin;
 using std::cout;
 
-int main() {
-    srand(time(NULL));
+void gameLoop() {
     Grid playground;
     char input;
     
-    playground.generate(); playground.generate();
-    playground.printGrid();
-
-    while (!playground.isGameOver()) {
-        input = getche();
-        // cin >> input;
-        cout << "\n";
-        if (input=='q') break;
-        if (input=='n') { 
-            playground.clearGrid();
-            playground.generate(); playground.generate();
-            playground.printGrid();
-        }
-        if (input=='w' || input=='a' || input=='s' || input=='d') {
-            switch (input) {
-            case 'w':
-                playground.moveUp();
-                break;
-            case 'a':
-                playground.moveLeft();
-                break;
-            case 's':
-                playground.moveDown();
-                break;
-            case 'd':
-                playground.moveRight();
+    while (true) {
+        playground.generate(); playground.generate();
+        playground.printGrid();
+        while (true) {
+            input = getche();
+            // cin >> input;
+            cout << "\n";
+            if (input=='q') 
+                return;
+            if (input=='n') { 
+                playground.clearGrid();
                 break;
             }
-            playground.generate();
-            playground.printGrid();
-        } 
+            if (input=='w' || input=='a' || input=='s' || input=='d') {
+                switch (input) {
+                case 'w':
+                    playground.moveUp();
+                    break;
+                case 'a':
+                    playground.moveLeft();
+                    break;
+                case 's':
+                    playground.moveDown();
+                    break;
+                case 'd':
+                    playground.moveRight();
+                    break;
+                default:
+                    break;
+                }
+                if (playground.isGameOver()) {
+                    cout << "You lost! Press 'n' to create a New Game: ";
+                    while (input!='q' && input!='n') {
+                        input = getche();
+                        if (input=='q')
+                            return;
+                        if (input=='n') { 
+                            playground.clearGrid();
+                            break;
+                        }
+                    }
+                }
+                playground.generate();
+                playground.printGrid();
+            } 
+        }
     }
+}
+
+int main() {
+    srand(time(NULL));
+    gameLoop();
     return 0;
 }
